e6ab5319b23d3ff545f4b09cd8c3a06e75ecc0c2
Gustavo Cotta <gmcotta34@gmail.com>
2019-11-24 15:45:54 -0300

    Store configuration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import '~/config/ReactotronConfig';
 import history from '~/services/history';
 import GlobalStyle from '~/styles/global';
 import Routes from '~/routes';
+import store from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
