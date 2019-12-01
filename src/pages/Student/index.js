16e81eefd64137621074ef744cd65a477f37d1be
Gustavo Cotta <gmcotta34@gmail.com>
2019-12-01 18:38:47 -0300

    Student page: api loading and search filter

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { MdAdd, MdSearch } from 'react-icons/md';
 import api from '~/services/api';
+import history from '~/services/history';
 import {
   Container,
   TableHeader,
@@ -12,12 +13,37 @@ import {
 } from './styles';
 
 export default function Student() {
+  const [students, setStudents] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    async function loadStudents() {
+      const response = await api.get('students', {
+        params: { name },
+      });
+      const studentsData = response.data;
+      setStudents(studentsData);
+      console.log(studentsData);
+    }
+    loadStudents();
+  }, [name]);
+
   function handleAddStudent() {
     alert('Add student');
   }
 
-  function handleFindStudent() {
-    alert('Find student');
+  function handleStudentName(e) {
+    setName(e.target.value);
+  }
+
+  function handleFindStudent(e) {
+    e.preventDefault();
+    alert(name);
+  }
+
+  function handleStudentEdit(id) {
+    console.log(`Edit student ${id}`);
+    history.push(`students/${id}`);
   }
 
   return (
@@ -31,45 +57,46 @@ export default function Student() {
           </AddButton>
           <SearchField onSubmit={handleFindStudent}>
             <MdSearch size={16} />
-            <input placeholder="Find student" />
+            <input
+              type="text"
+              placeholder="Find student"
+              onChange={handleStudentName}
+            />
           </SearchField>
         </ButtonArea>
       </TableHeader>
 
-      <Table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>E-mail</th>
-            <th>Age</th>
-            <th> </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Darius Cummings</td>
-            <td>example@rocketseat.com.br</td>
-            <td>18</td>
-            <td id="options">
-              <Link to="/">edit</Link>
-              <button type="button" onClick={() => {}}>
-                remove
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>Darius Cummings</td>
-            <td>example@rocketseat.com.br</td>
-            <td>18</td>
-            <td id="options">
-              <Link to="/">edit</Link>
-              <button type="button" onClick={() => {}}>
-                remove
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+      {students.length ? (
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>E-mail</th>
+              <th>Age</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map(student => (
+              <tr key={student.id}>
+                <td>{student.name}</td>
+                <td>{student.email}</td>
+                <td>{student.age}</td>
+                <td id="options">
+                  <Link to="/">edit</Link>
+                  <button type="button" onClick={() => {}}>
+                    remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <div>
+          <h1>No students found</h1>
+        </div>
+      )}
     </Container>
   );
 }
