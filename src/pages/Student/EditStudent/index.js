1a39e67766e6bfbfd89b92d07cc025db4088f29f
Gustavo Cotta <gmcotta34@gmail.com>
2019-12-04 21:54:43 -0300

    Student edit page: styling and editing onfront-end

--- src/pages/Student/EditStudent/index.js
+++ src/pages/Student/EditStudent/index.js
@@ -1,7 +1,99 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { MdArrowBack, MdSave } from 'react-icons/md';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
+import {
+  Container,
+  FormHeader,
+  FormContent,
+  ButtonArea,
+  BackButton,
+  SaveButton,
+  BodyInfo,
+} from './styles';
+import api from '~/services/api';
+import history from '~/services/history';
 
 export default function EditStudent() {
-  return <h1>EditStudent</h1>;
+  const [student, setStudent] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loadStudents() {
+      const response = await api.get('students');
+      const studentsData = response.data;
+      console.log(studentsData);
+      const currentStudent = studentsData.find(s => s.id === Number(id));
+      console.log(currentStudent);
+
+      setStudent(currentStudent);
+    }
+    loadStudents();
+  }, [id]);
+
+  const schema = Yup.object().shape({
+    name: Yup.string(),
+    email: Yup.string().email(),
+    age: Yup.number().integer(),
+    weight: Yup.number(),
+    height: Yup.number(),
+  });
+
+  async function updateStudent(data) {
+    try {
+      await api.put(`students/${id}`, data);
+      toast.success('The student has been updated!');
+      history.push('/');
+    } catch (error) {
+      console.tron.log(error);
+      toast.error(error);
+    }
+    console.tron.log(data);
+  }
+
+  function goBack() {
+    history.push('/');
+  }
+
+  return (
+    <Container>
+      <Form schema={schema} initialData={student} onSubmit={updateStudent}>
+        <FormHeader>
+          <span>Edit Student</span>
+          <ButtonArea>
+            <BackButton type="button" onClick={goBack}>
+              <MdArrowBack size={16} />
+              <span>Back</span>
+            </BackButton>
+            <SaveButton type="submit">
+              <MdSave size={16} />
+              <span>Save</span>
+            </SaveButton>
+          </ButtonArea>
+        </FormHeader>
+        <FormContent>
+          <strong>Full name</strong>
+          <Input name="name" type="text" placeholder="John Doe" />
+          <strong>E-mail address</strong>
+          <Input name="email" type="email" placeholder="example@email.com" />
+          <BodyInfo>
+            <div>
+              <strong>Age</strong>
+              <Input name="age" type="number" />
+            </div>
+            <div>
+              <strong>Weight (in kg)</strong>
+              <Input name="weight" type="text" />
+            </div>
+            <div>
+              <strong>Height (in m)</strong>
+              <Input name="height" type="text" />
+            </div>
+          </BodyInfo>
+        </FormContent>
+      </Form>
+    </Container>
+  );
 }
---
+++ src/pages/Student/EditStudent/styles.js
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
+
+export const FormHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 30px;
+
+  span {
+    font-size: 24px;
+    font-weight: bold;
+    color: #444;
+  }
+`;
+
+export const FormContent = styled.div`
+  background-color: #fff;
+  width: 100%;
+  margin: 20px auto;
+  border-radius: 4px;
+  padding: 15px 30px 30px;
+  display: flex;
+  flex-direction: column;
+
+  strong {
+    text-transform: uppercase;
+    font-weight: bold;
+    margin-top: 15px;
+  }
+
+  span {
+    color: #ee4d64;
+  }
+
+  input {
+    height: 45px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 15px;
+    margin: 8px 0px;
+
+    &::placeholder {
+      color: #999;
+    }
+  }
+`;
+
+export const BodyInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    width: 270px;
+  }
+`;
+
+export const ButtonArea = styled.div`
+  display: flex;
+`;
+
+export const BackButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 36px;
+  padding: 10px;
+  margin-right: 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #ddd;
+  color: #fff;
+
+  span {
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
+    margin-left: 7px;
+  }
+`;
+
+export const SaveButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 36px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #ee4d64;
+  color: #fff;
+
+  span {
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
+    margin-left: 7px;
+  }
+`;
