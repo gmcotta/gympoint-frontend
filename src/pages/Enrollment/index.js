a4056236533dec1c8bd83638166899f154164bfe
Gustavo Cotta <gmcotta34@gmail.com>
2019-12-08 20:59:20 -0300

    Enrollment Page: styling and backend integration

--- src/pages/Enrollment/index.js
+++ src/pages/Enrollment/index.js
@@ -1,7 +1,123 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import { MdAdd, MdCheckCircle } from 'react-icons/md';
+import { parseISO, format } from 'date-fns';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+import history from '~/services/history';
+import {
+  Container,
+  TableHeader,
+  ButtonArea,
+  AddButton,
+  Table,
+  EditButton,
+  RemoveButton,
+  NoEnrollmentArea,
+} from './styles';
 
 export default function Enrollment() {
-  return <h1>Enrollment</h1>;
+  const [enrollments, setEnrollments] = useState([]);
+
+  useEffect(() => {
+    async function loadEnrollments() {
+      const response = await api.get('enrollments');
+      const enrollmentsData = response.data.filter(e => e.student_id !== null);
+      const newEnrollment = enrollmentsData.map(e => ({
+        ...e,
+        formattedStartDate: format(parseISO(e.start_date), "MMMM' 'd', 'yyyy"),
+        formattedEndDate: format(parseISO(e.end_date), "MMMM' 'd', 'yyyy"),
+      }));
+      console.log(newEnrollment);
+      setEnrollments(newEnrollment);
+    }
+    loadEnrollments();
+  }, []);
+
+  function handleAddEnrollment() {
+    history.push('enrollments/new');
+  }
+
+  function handleEnrollmentEdit(id) {
+    history.push(`enrollments/${id}`);
+  }
+
+  async function handleEnrollmentRemove(enrollment_id) {
+    if (window.confirm('Are you sure you wanna remove this enrollment?')) {
+      try {
+        await api.delete(`enrollments/${enrollment_id}`);
+        const newEnrollments = enrollments.filter(
+          enrollment => enrollment.id !== enrollment_id
+        );
+        setEnrollments(newEnrollments);
+        toast.success('Enrollment removed successfully.');
+      } catch (error) {
+        toast.error('An error occurred. Plase, try again later.');
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <TableHeader>
+        <span>Managing Enrollments</span>
+        <ButtonArea>
+          <AddButton onClick={handleAddEnrollment}>
+            <MdAdd size={16} />
+            <span>Add</span>
+          </AddButton>
+        </ButtonArea>
+      </TableHeader>
+
+      {enrollments.length ? (
+        <Table>
+          <thead>
+            <tr>
+              <th>Student</th>
+              <th>Plan</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Active</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {enrollments.map(enrollment => (
+              <tr key={enrollment.id}>
+                <td>{enrollment.Student.name}</td>
+                <td>{enrollment.Plan.title}</td>
+                <td>{enrollment.formattedStartDate}</td>
+                <td>{enrollment.formattedEndDate}</td>
+                <td>
+                  <MdCheckCircle
+                    size={16}
+                    color={enrollment.active ? '#42cb59' : '#ddd'}
+                  />
+                </td>
+                <td id="options">
+                  <EditButton
+                    type="button"
+                    onClick={() => handleEnrollmentEdit(enrollment.id)}
+                  >
+                    edit
+                  </EditButton>
+                  <RemoveButton
+                    type="button"
+                    onClick={() => {
+                      handleEnrollmentRemove(enrollment.id);
+                    }}
+                  >
+                    remove
+                  </RemoveButton>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <NoEnrollmentArea>
+          <h1>No enrollments found</h1>
+        </NoEnrollmentArea>
+      )}
+    </Container>
+  );
 }
---
+++ src/pages/Enrollment/styles.js
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1260px;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
+
+export const TableHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 30px;
+
+  span {
+    font-size: 24px;
+    font-weight: bold;
+    color: #444;
+  }
+`;
+
+export const ButtonArea = styled.div`
+  display: flex;
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 36px;
+  padding: 10px;
+  margin-right: 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #ee4d64;
+  color: #fff;
+
+  span {
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
+    margin-left: 7px;
+  }
+`;
+
+export const SearchField = styled.form`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  svg {
+    color: #999;
+    position: absolute;
+    left: 10px;
+  }
+
+  input {
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 16px 16px 16px 36px;
+    max-height: 36px;
+    color: #999;
+  }
+`;
+
+export const Table = styled.table`
+  background-color: #fff;
+  width: 100%;
+  margin: 20px auto;
+  border-radius: 4px;
+  padding: 20px;
+
+  th {
+    text-transform: uppercase;
+    text-align: left;
+  }
+
+  td {
+    color: #666;
+    padding: 16px 0;
+  }
+
+  td#options {
+    text-align: right;
+  }
+`;
+
+export const EditButton = styled.button`
+  color: #4d85ee;
+  margin-left: 25px;
+  background: none;
+  border: none;
+`;
+
+export const RemoveButton = styled.button`
+  color: #de3b3b;
+  margin-left: 25px;
+  background: none;
+  border: none;
+`;
+
+export const NoEnrollmentArea = styled.div`
+  background-color: #fff;
+  width: 100%;
+  margin: 20px auto;
+  border-radius: 4px;
+  padding: 20px;
+  text-align: center;
+  color: #999;
+`;
